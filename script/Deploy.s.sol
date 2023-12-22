97cde5935d75e3fa875987f1621402b2d3ae9f03
ZeframLou <zeframlou@gmail.com>
2023-12-22 12:37:39 -0800

    build: deploy scripts

--- script/Deploy.s.sol
+++ script/Deploy.s.sol
@@ -1,22 +1,47 @@
 // SPDX-License-Identifier: AGPL-3.0
 pragma solidity ^0.8.13;
 
+import {LibString} from "solady/src/utils/LibString.sol";
+import {SafeCastLib} from "solady/src/utils/SafeCastLib.sol";
+
 import {CREATE3Script} from "./base/CREATE3Script.sol";
+
+import {BunniHub} from "../src/BunniHub.sol";
 import {BunniHook} from "../src/BunniHook.sol";
 
 contract DeployScript is CREATE3Script {
+    using LibString for uint256;
+    using SafeCastLib for uint256;
+
     constructor() CREATE3Script(vm.envString("VERSION")) {}
 
-    function run() external returns (BunniHook c) {
+    function run() external returns (BunniHub hub, BunniHook hook) {
         uint256 deployerPrivateKey = uint256(vm.envBytes32("PRIVATE_KEY"));
 
-        uint256 param = 123;
+        address poolManager = vm.envAddress("POOL_MAMAGER");
+        address weth = vm.envAddress(string.concat("WETH_", block.chainid.toString()));
+        address permit2 = vm.envAddress("PERMIT2");
+        address owner = vm.envAddress(string.concat("OWNER_", block.chainid.toString()));
+        address hookFeesRecipient = vm.envAddress(string.concat("HOOK_FEES_RECIPIENT_", block.chainid.toString()));
+        uint96 hookFeesModifier = vm.envUint("HOOK_FEES_MODIFIER").toUint96();
 
         vm.startBroadcast(deployerPrivateKey);
 
-        c = BunniHook(
+        hub = BunniHub(
+            payable(
+                create3.deploy(
+                    getCreate3ContractSalt("BunniHub"),
+                    bytes.concat(type(BunniHub).creationCode, abi.encode(poolManager, weth, permit2))
+                )
+            )
+        );
+        hook = BunniHook(
             create3.deploy(
-                getCreate3ContractSalt("BunniHook"), bytes.concat(type(BunniHook).creationCode, abi.encode(param))
+                getCreate3ContractSalt("BunniHook"),
+                bytes.concat(
+                    type(BunniHook).creationCode,
+                    abi.encode(poolManager, hub, owner, hookFeesRecipient, hookFeesModifier)
+                )
             )
         );
 
---
+++ script/DeployLDFs.s.sol
@@ -0,0 +1,51 @@
+// SPDX-License-Identifier: AGPL-3.0
+pragma solidity ^0.8.13;
+
+import {CREATE3Script} from "./base/CREATE3Script.sol";
+
+import {DiscreteLaplaceDistribution} from "../src/ldf/DiscreteLaplaceDistribution.sol";
+import {GeometricDistribution} from "../src/ldf/GeometricDistribution.sol";
+import {DoubleGeometricDistribution} from "../src/ldf/DoubleGeometricDistribution.sol";
+
+contract DeployLDFsScript is CREATE3Script {
+    constructor() CREATE3Script(vm.envString("VERSION")) {}
+
+    function run()
+        external
+        returns (
+            DiscreteLaplaceDistribution discreteLaplace,
+            GeometricDistribution geometric,
+            DoubleGeometricDistribution doubleGeometric
+        )
+    {
+        uint256 deployerPrivateKey = uint256(vm.envBytes32("PRIVATE_KEY"));
+
+        vm.startBroadcast(deployerPrivateKey);
+
+        discreteLaplace = DiscreteLaplaceDistribution(
+            payable(
+                create3.deploy(
+                    getCreate3ContractSalt("DiscreteLaplaceDistribution"),
+                    type(DiscreteLaplaceDistribution).creationCode
+                )
+            )
+        );
+        geometric = GeometricDistribution(
+            payable(
+                create3.deploy(
+                    getCreate3ContractSalt("GeometricDistribution"), type(GeometricDistribution).creationCode
+                )
+            )
+        );
+        doubleGeometric = DoubleGeometricDistribution(
+            payable(
+                create3.deploy(
+                    getCreate3ContractSalt("DoubleGeometricDistribution"),
+                    type(DoubleGeometricDistribution).creationCode
+                )
+            )
+        );
+
+        vm.stopBroadcast();
+    }
+}
