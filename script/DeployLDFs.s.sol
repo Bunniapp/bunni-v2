ca0a74e3f842f1bda23763f4a5d6b17b3d63e64f
ZeframLou <zeframlou@gmail.com>
2024-03-05 13:52:16 -0800

    deploy carpeted LDFs

--- script/DeployLDFs.s.sol
+++ script/DeployLDFs.s.sol
@@ -5,11 +5,21 @@ import {CREATE3Script} from "./base/CREATE3Script.sol";
 
 import {GeometricDistribution} from "../src/ldf/GeometricDistribution.sol";
 import {DoubleGeometricDistribution} from "../src/ldf/DoubleGeometricDistribution.sol";
+import {CarpetedGeometricDistribution} from "../src/ldf/CarpetedGeometricDistribution.sol";
+import {CarpetedDoubleGeometricDistribution} from "../src/ldf/CarpetedDoubleGeometricDistribution.sol";
 
 contract DeployLDFsScript is CREATE3Script {
     constructor() CREATE3Script(vm.envString("VERSION")) {}
 
-    function run() external returns (GeometricDistribution geometric, DoubleGeometricDistribution doubleGeometric) {
+    function run()
+        external
+        returns (
+            GeometricDistribution geometric,
+            DoubleGeometricDistribution doubleGeometric,
+            CarpetedGeometricDistribution carpetedGeometric,
+            CarpetedDoubleGeometricDistribution carpetedDoubleGeometric
+        )
+    {
         uint256 deployerPrivateKey = uint256(vm.envBytes32("PRIVATE_KEY"));
 
         vm.startBroadcast(deployerPrivateKey);
@@ -27,6 +37,24 @@ contract DeployLDFsScript is CREATE3Script {
                 create3.deploy(
                     getCreate3ContractSalt("DoubleGeometricDistribution"),
                     type(DoubleGeometricDistribution).creationCode
+                )
+            )
+        );
+
+        carpetedGeometric = CarpetedGeometricDistribution(
+            payable(
+                create3.deploy(
+                    getCreate3ContractSalt("CarpetedGeometricDistribution"),
+                    type(CarpetedGeometricDistribution).creationCode
+                )
+            )
+        );
+
+        carpetedDoubleGeometric = CarpetedDoubleGeometricDistribution(
+            payable(
+                create3.deploy(
+                    getCreate3ContractSalt("CarpetedDoubleGeometricDistribution"),
+                    type(CarpetedDoubleGeometricDistribution).creationCode
                 )
             )
         );
